2b8ee3509f3eca5d5fa2f0201dada2c577b2a1e1
GUhxtavo <gustavobrito042018@gmail.com>
2026-04-30 14:37:55 -0300

    Add files via upload

---
+++ exercício1.py
@@ -0,0 +1,10 @@
+# Exercício 1: Desenvolva um algoritmo que solicite ao usuário dois números inteiros e dois números float. Imprima a soma destes quatro números na tela.
+
+n1 = int(input("Digite um número inteiro:"))
+n2 = int(input("Digite um segundo número inteiro:"))
+n3 = float(input("Digite um número float:"))
+n4 = float(input("Digite um segundo número float:"))
+
+resultado = n1 + n2 + n3 + n4 
+
+print(resultado)
---
+++ exercício2.py
@@ -0,0 +1,8 @@
+dias = int(input("digite uma quantidade de dias:"))
+horas = int(float("digite as horas:"))
+minutos = int(float("digite os minutos:"))
+segundos = int(input("digite os segundos:"))
+
+segundos_restantes = dias / 86.400 
+segundos_restantes = horas / 3.600
+segundos_restantes = minutos 
