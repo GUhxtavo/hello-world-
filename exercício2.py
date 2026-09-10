0e145b1c10f2713e9d52bfbb629b3d1782b6de19
GUhxtavo <gustavobrito042018@gmail.com>
2026-09-10 15:02:55 -0300

    Delete exercício2.py

--- exercício2.py
+++
@@ -1,8 +0,0 @@
-dias = int(input("digite uma quantidade de dias:"))
-horas = int(float("digite as horas:"))
-minutos = int(float("digite os minutos:"))
-segundos = int(input("digite os segundos:"))
-
-segundos_restantes = dias / 86.400 
-segundos_restantes = horas / 3.600
-segundos_restantes = minutos 
